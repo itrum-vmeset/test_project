9ee05d48f08bf220369a6f8ffceb0715730432bd
Denis Serdyuk <serdyuk.itrum345@gmail.com>
2023-04-10 18:47:57 +0300

    timeLimit commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,13 @@
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, makeStyles } from '@mui/material'
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function Sidebar() {
 
     const isAuth = true
     const navigate = useNavigate()
+    const location = useLocation()
 
     interface IMenu {
         route: string,
@@ -15,21 +16,23 @@ function Sidebar() {
     }
 
     const menuItems: IMenu[] = [
-        {route: 'products', name: 'Продукты', icon: <StarBorderIcon />},
-        {route: 'clients', name: 'Клиенты', icon: <StarBorderIcon />,},
-        {route: 'categories', name: 'Категории', icon: <StarBorderIcon />},
-        {route: 'cities', name: 'Города', icon: <StarBorderIcon />,},
-        {route: 'brands', name: 'Бренды', icon: <StarBorderIcon />,},
-        {route: 'protocols', name: 'Протоколы', icon: <StarBorderIcon />,},
+        {route: '/products', name: 'Продукты', icon: <StarBorderIcon />},
+        {route: '/clients', name: 'Клиенты', icon: <StarBorderIcon />,},
+        {route: '/categories', name: 'Категории', icon: <StarBorderIcon />},
+        {route: '/cities', name: 'Города', icon: <StarBorderIcon />,},
+        {route: '/brands', name: 'Бренды', icon: <StarBorderIcon />,},
+        {route: '/protocols', name: 'Протоколы', icon: <StarBorderIcon />,},
         {route: '/orders', name: 'Заказы', icon: <StarBorderIcon />,},
-        {route: 'banners', name: 'Баннеры', icon: <StarBorderIcon />,},
-        {route: 'seminars', name: 'Семинары', icon: <StarBorderIcon />,},
-        {route: 'promocode', name: 'Промокоды', icon: <StarBorderIcon />,},
+        {route: '/banners', name: 'Баннеры', icon: <StarBorderIcon />,},
+        {route: '/seminars', name: 'Семинары', icon: <StarBorderIcon />,},
+        {route: '/promocode', name: 'Промокоды', icon: <StarBorderIcon />,},
     ]
 
     if(!isAuth) {
         return <></>
     }
+
+    
 
   return (
     <List>
@@ -37,7 +40,14 @@ function Sidebar() {
             <ListItem key={route} disablePadding
                 onClick={() => navigate(route)}
             >
-                <ListItemButton>
+                <ListItemButton
+                    selected={route === location.pathname ? true : false}
+                    sx={{
+                        ':selected': {
+                            bgcolor: 'red',
+                        },
+                    }}
+                >
                     <ListItemIcon>
                         {icon}
                     </ListItemIcon>
